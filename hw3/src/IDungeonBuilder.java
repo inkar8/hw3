c98cd44716b59c2ec45ca04f75bdf6cdeb0a70a9
Inkar Esteu <inkaresteu@gmail.com>
2025-02-28 20:59:37 +0500

    Delete hw3 directory

--- hw3/src/IDungeonBuilder.java
+++
@@ -1,7 +0,0 @@
-// IDungeonBuilder.java (Interface)
-public interface IDungeonBuilder {
-    IDungeonBuilder setDungeonName(String name);
-    IDungeonBuilder addRoom(Dungeon.Room room);
-    IDungeonBuilder addNPC(Dungeon.NPC npc);
-    Dungeon build();
-}
